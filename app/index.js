f56296837e8c203c5853d71820ba56cbdebb742b
theGrandmaster05 <israelakintunde005@gmail.com>
2018-09-06 00:31:15 +0100

    Tried fixing unresolvable css error

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hello, Israel
+                Hello World
             </div>
         )
     }
