1966ba722f7049316a38623f0d1062db43f7710a
theGrandmaster05 <israelakintunde005@gmail.com>
2018-09-06 00:45:26 +0100

    Added webpack-server

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World
+                Hello, Israel Akintunde
             </div>
         )
     }
