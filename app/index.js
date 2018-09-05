f1059afda2514a9c21119713978b9375d557d5ee
theGrandmaster05 <israelakintunde005@gmail.com>
2018-09-05 12:34:21 +0100

    configuring webpack and babel

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('index.css');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, Israel
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,document.getElementById('app')
+);
