151bd5740265b13617f28c17d9854b4354e069fd
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-01 09:52:31 +0100

    Changes

--- app/index.js
+++ app/index.js
@@ -2,19 +2,23 @@ let React = require('react');
 let ReactDOM = require('react-dom');
 require('./index.css');
 
+let listOfCourses = [
+    'English','Yoruba','French','Portugese','Spanish'
+];
+
 class User extends React.Component {
     render() {
         return(
             <div>
                 <h1>{this.props.username}</h1>
-                <p>The courses to be sold to them are as listed below</p>
+                <p> The courses you are offering are as listed below </p>
                 <ul>
-                    {}
+                    {this.props.courses.map( n => <li> {n} </li>)}
                 </ul>
             </div>
         )
     }
 }
 ReactDOM.render(
-    <App username=/>,document.getElementById('app')
+    <User username={"theGrandmaster"} courses={listOfCourses}/>,document.getElementById('app')
 );
