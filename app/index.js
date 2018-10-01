10089198dea5bc41ea4703bbbacbe8986d6f3bd1
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-01 22:20:41 +0100

    Committed new changes

--- app/index.js
+++ app/index.js
@@ -3,17 +3,27 @@ let ReactDOM = require('react-dom');
 require('./index.css');
 
 let listOfCourses = [
-    'English','Yoruba','French','Portugese','Spanish'
+    { name: 'Israel', friend: false },
+    { name: 'Temi', friend: true },
+    { name: 'Toba', friend: false },
+    { name: 'Jade', friend: true }
 ];
 
 class User extends React.Component {
     render() {
+        let friends = this.props.courses.filter(name => name.friend === true);
+        let notFriend = this.props.courses.filter(name => name.friend !== true);
         return(
             <div>
-                <h1>{this.props.username}</h1>
+                <h1> {this.props.username} </h1>
+                <p> Your friends are listed below </p>
+                <ul>
+                    { friends.map(name => <li key={ name.name }> { name.name } </li>) }
+                </ul>
+                <h1> {this.props.username} </h1>
                 <p> The courses you are offering are as listed below </p>
                 <ul>
-                    {this.props.courses.map( n => <li> {n} </li>)}
+                    { notFriend.map(name => <li key={ name.name }> { name.name } </li>) }
                 </ul>
             </div>
         )
