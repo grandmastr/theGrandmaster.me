ac026604a68a3a1dc3ef2751453daa6da393a564
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-05 05:07:31 +0100

    Added React Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,16 @@
 let React = require('react');
 let Popular = require('./Popular');
+let ReactRouter = require('react-router-dom');
+let Router = ReactRouter.BrowserRouter;
+let Route = ReactRouter.Route;
 class App extends React.Component {
     render() {
         return(
-            <div className="container">
-                <Popular />
-            </div>
+            <Router>
+                <div className="container">
+                    <Route path="/popular" component={Popular} />
+                </div>
+            </Router>
         )
     }
 }
