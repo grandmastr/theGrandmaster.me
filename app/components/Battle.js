7adcdeb450c7ea0b4dcb05774b6fab0fc2f8cfe9
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-08 21:10:59 +0100

    Form validation and Encapsulation

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,18 @@
 let React = require('react');
 
+
+class PlayerInput extends React.Component {
+    render() {
+        return(
+
+        )
+    }
+}
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onsubmit: PropTypes.func.isRequired
+};
 class Battle extends React.Component {
     constructor(props) {
         super(props);
@@ -13,14 +26,22 @@ class Battle extends React.Component {
     }
     handleSubmit(id,username) {
         this.setState(() => {
-            var newState = {};
+            let newState = {};
             newState[`${id}Name`] = username;
             newState[`${id}Image`] = `https://github.com/${username}.png?size=200`;
         })
     }
     render() {
+        let playerOneName = this.state.playerOneName;
+        let playerTwoName = this.state.playerTwoName;
         return (
+            <div className="row">
+                { !playerOneName &&
+                <PlayerInput id="playerOne" label="Player One" onSubmit={this.handleSubmit}/> }
 
+                { !playerTwoName &&
+                <playerInut id="playerTwo" label="player Two" onSubmit={this.handleSubmit}/> }
+            </div>
         )
     }
 }
