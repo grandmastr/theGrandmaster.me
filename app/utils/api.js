0a66de4e240ebd2d080f3fce3ebe6b0b814a20e4
theGrandmaster05 <israelakintunde005@gmail.com>
2018-10-13 04:34:44 +0100

    axios and promises

--- app/utils/api.js
+++ app/utils/api.js
@@ -11,7 +11,7 @@ const getRepos = username => {
 };
 module.exports = {
     fetchPopularRepos: language => {
-        let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language: ${language} &sort=stars&order=desc&type=Repositories&per_page=100`);
+        let encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language: ${language} &sort=stars&order=desc&type=Repositories&per_page=200`);
         return axios.get(encodedURI)
             .then(response => {
                return response.data.items
